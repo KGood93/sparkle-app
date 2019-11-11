223d38febd0da226a1db091f3e194b6b09de39a9
KGood93 <goodreau.kath@gmail.com>
2019-11-10 22:04:35 -0600

    Add Journal

---
+++ src/Journal/Journal.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Journal.css';
+
+class Journal extends React.Component {
+
+    render() {
+        return (
+          <div className="main">
+            <section className="entries">
+                <div className="entryOdd">
+                    <h3 className="entryName">Entry Title 1</h3>
+                    <h4 className="entryDate">Date</h4>
+                    <button type="button" className="Open">Open</button>
+                    <button type="button" className="Delete">Delete</button>
+                </div>
+                <div className="entryEven">
+                    <h3 className="entryName">Entry Title 2</h3>
+                    <h4 className="entryDate">Date</h4>
+                    <button type="button" className="Open">Open</button>
+                    <button type="button" className="Delete">Delete</button>
+                </div>
+                <div className="entryOdd">
+                    <h3 className="entryName">Entry Title 3</h3>
+                    <h4 className="entryDate">Date</h4>
+                    <button type="button" className="Open">Open</button>
+                    <button type="button" className="Delete">Delete</button>
+                </div>
+                <div className="entryEven">
+                    <h3 className="entryName">Entry Title 4</h3>
+                    <h4 className="entryDate">Date</h4>
+                    <button type="button" className="Open">Open</button>
+                    <button type="button" className="Delete">Delete</button>
+                </div>
+            </section>
+          </div>
+        )
+    }
+}
+export default Journal;

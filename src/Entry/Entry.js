6313a250608f4207ee27d9decb89491c64dac67d
KGood93 <goodreau.kath@gmail.com>
2019-11-10 21:56:38 -0600

    Add Basic Entry

--- src/Entry/Entry.js
+++ src/Entry/Entry.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './Entry.css';
 
-function Entry() {
-  return (
-    <div className="App">
+class Entry extends React.Component {
 
-    </div>
-  );
+    render() {
+        return (
+          <div className="main">
+            <div className="entryMain">
+              <section className="motivQuote">
+                <h2>Motivation Quote Prompt</h2>
+                <h4 className="quoteBy">Quote Name</h4>
+              </section>
+      
+              <section>
+                <textarea></textarea>
+              </section>
+            </div>
+      
+            <div className="entryButtons">
+              <section className="buttons">
+                  <button type="button" className="clear">Clear Entry</button>
+                  <button type="button" className="edit">Edit Entry</button>
+                  <button type="button" className="save">Save Entry</button>
+              </section>
+            </div>
+          </div>
+        )
+    }
 }
-
 export default Entry;

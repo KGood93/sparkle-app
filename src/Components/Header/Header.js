0e68ca4fed766e1b303d97dad543dd96d4dc91a8
KGood93 <goodreau.kath@gmail.com>
2019-11-24 08:21:01 -0600

    Add token service

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 //import { Hyph } from '../Utils/Utils'
 //import TokenService from '../../services/token-service'
 import './Header.css'

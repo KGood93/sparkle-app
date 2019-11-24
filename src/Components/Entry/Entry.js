483235145c8b5a2f55eff150f946a3a53c8178ce
KGood93 <goodreau.kath@gmail.com>
2019-11-24 15:07:57 -0600

    Update Entry

--- src/Components/Entry/Entry.js
+++ src/Components/Entry/Entry.js
@@ -1,21 +1,61 @@
-import React, {Component} from 'react';
-import EntryForm from '../EntryForm/EntryForm';
-import Quote from '../Quote/Quote';
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+import ApiContext from '.../ApiContext'
+import config from '.../config'
 import './Entry.css';
 
 
 class Entry extends Component {
+  static contextType = ApiContext;
 
-    render() {
-        return (
-          <div className="main">
-            <nav role="navigation">
-              <a href="landing.html" class="homeNav">Home</a>
-            </nav>
-            <Quote />
-            <EntryForm />
-          </div>
-        )
+  handleClickDelete = e => {
+    e.preventDefault()
+    const entryId = this.props.id
+    
+    fetch(`${config.API_ENDPOINT}/entry/${entryId}`, {
+      method: 'DELETE', 
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+      .then(res => {
+        if(!res.ok)
+          return res.json().then(e => Promise.reject(e))
+          return res.json()
+      })
+      .then(() => {
+        this.context.deleteEntry(entryId)
+        this.props.onDeleteEntry(entryId)
+      })
+      .catch(error => {
+        console.error({ error })
+      })
+  }
+  
+  render() {
+    const { name, id, modified } = this.props
+    return (
+    <div className='entry'>
+      <h2 className='entryEven'>
+        <Link to={`/entry/${id}`}>
+          {name}
+        </Link>
+      </h2>
+      <button className='Note_delete' type='button' onClick={this.handleClickDelete}>
+        {' '}
+        Remove
+      </button>
+      <div className='Note_dates'>
+        <div className='Note_dates-modified'>
+          Modified
+          {' '}
+          <span className='Date'>
+            {format(modified, 'Do MMM YYYY')}
+          </span>
+        </div>
+      </div>
+    </div>
+  )
     }
 }
 export default Entry;

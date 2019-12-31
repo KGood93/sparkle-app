c6bb3e425c71c21b7a93df7e9d7f146eb2d51a8f
KGood93 <goodreau.kath@gmail.com>
2019-12-30 20:59:55 -0600

    displays entry based on Id

--- src/Components/EntryMain/EntryMain.js
+++ src/Components/EntryMain/EntryMain.js
@@ -3,22 +3,17 @@ import './EntryMain.css';
 import Entry from '../Entry/Entry'
 import ApiContext from '../../ApiContext'
 import { allEntries } from '../../STORE';
-import {findEntry} from '../../entry-helpers'
 
 //similar to notePageMain
-//const entries = allEntries().entries[1]
-//console.log(entries)
-
-//need to set context
 
 class EntryMain extends Component {
   static contextType = ApiContext
   
   render() {
-    const {entries=[]} = this.context 
     const {entryId} = this.props.match.params
     console.log(entryId)
-    const entry = findEntry(entries, entryId) || {content: ''}
+    const entry = allEntries().entries[entryId - 1]
+    //const entry = findEntry(entries, entryId) || {content: ''}
     return (
         <div className='EntryPageMain'>
           <Entry

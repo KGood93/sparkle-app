b7a8b3135f83916425cd1433909975f3cb76e848
KGood93 <goodreau.kath@gmail.com>
2020-02-04 20:51:59 -0600

    remove backslash

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://warm-escarpment-52336.herokuapp.com/',
+    API_ENDPOINT: 'https://warm-escarpment-52336.herokuapp.com',
     TOKEN_KEY: 'sparkle-client-auth-token',
   }

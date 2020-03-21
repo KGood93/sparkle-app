960d3d8a6234e4186e2519f73568bf88860372d6
KGood93 <goodreau.kath@gmail.com>
2020-03-20 22:47:51 -0500

    update Endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    //API_ENDPOINT: 'https://warm-escarpment-52336.herokuapp.com',
-    API_ENDPOINT: 'http://localhost:8000',
+    API_ENDPOINT: 'https://warm-escarpment-52336.herokuapp.com',
+    //API_ENDPOINT: 'http://localhost:8000',
     TOKEN_KEY: 'sparkle-client-auth-token',
   }
